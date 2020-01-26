a73c941dfaa6548c251d364530c461cd12e952d8
Pedja <vpedja12@gmail.com>
2020-01-26 00:57:50 -0800

    setup server.js file

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 var express = require("express");
 var mongoose = require("mongoose");
+var logger = require("morgan");
 
 var app = express();
-var PORT = process.env.PORT || 3001;
+var PORT = process.env.PORT || 3021;
 
+app.use(logger("dev"));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/serviceWorkerDemo", {
-  useNewUrlParser: true
+  useNewUrlParser: true,
+  useFindAndModify: false
 });
 
 app.listen(PORT, function() {
